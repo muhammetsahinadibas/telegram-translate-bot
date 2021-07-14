e73a94e058b013974e62e861b03b90bec716789a
Muhammet Şahin ADİBAŞ <59294411+muhammetsahinadibas@users.noreply.github.com>
2021-07-14 13:09:22 +0300

    Add files via upload

---
+++ Turkish_to_English.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import telebot
+from googletrans import Translator
+
+translator = Translator()
+bot = telebot.TeleBot(" ") # Enter Telegram bot key !!
+
+@bot.message_handler(commands=['start']) 
+def send_welcome(message):
+	bot.reply_to(message, "Welcome, Type in the word or text you want to translate.")
+
+@bot.message_handler(commands=['help']) 
+def send_help(message):
+    bot.reply_to(message, "Type in the word or text you want to translate.")
+
+@bot.message_handler(content_types = ['text']) 
+def decoder(message):
+    data = message.text
+    value = translator.translate(text=data, src="tr", dest="en")
+    bot.send_message(message.chat.id, value.text) 
+
+bot.polling() 
+
+# - Developer: Muhammet Sahin Adibas
+# - Twitter: twitter.com/muhammetadibas 
+# - Blog: muhammetsahinadibas.com.tr
+# - Github: github.com/muhammetsahinadibas 
